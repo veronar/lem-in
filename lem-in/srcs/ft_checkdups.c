4d030531069a310ea2fcf14c8799923619185e2a
vesingh <vesingh@student.wethinkcode.co.za>
2019-08-28 14:16:08 +0200

    checks for dup coords

--- lem-in/srcs/ft_checkdups.c
+++ lem-in/srcs/ft_checkdups.c
@@ -6,18 +6,44 @@
 /*   By: vesingh <vesingh@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/27 08:16:56 by vesingh           #+#    #+#             */
-/*   Updated: 2019/08/28 12:00:08 by vesingh          ###   ########.fr       */
+/*   Updated: 2019/08/28 14:15:34 by vesingh          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lemin.h"
 
 /*
-** ft_checkdups: check for duplicate rooms
+** ft_dupcoords: check for duplicate room coordinates, ie two rooms in the
+** same place.
 ** if true, error.
 */
 
-int			ft_checkdups(t_room **head)
+int			ft_dupcoords(t_room **head)
+{
+	t_room	*start;
+	t_room	*second;
+
+	start = *head;
+	while (start != NULL)
+	{
+		second = start->next;
+		while (second != NULL)
+		{
+			if (start->x == second->x && start->y == second->y)
+				return (-1);
+			second = second->next;
+		}
+		start = start->next;
+	}
+	return (1);
+}
+
+/*
+** ft_dupnames: check for duplicate rooms
+** if true, error.
+*/
+
+int			ft_dupnames(t_room **head)
 {
 	t_room	*start;
 	t_room	*second;
@@ -35,4 +61,16 @@ int			ft_checkdups(t_room **head)
 		start = start->next;
 	}
 	return (1);
+}
+
+/*
+** ft_checkdups: calls to check dup name & coords
+*/
+
+int			ft_checkdups(t_room **head)
+{
+	if (ft_dupnames(head) == -1 || ft_dupcoords(head) == -1)
+		return (-1);
+	else
+		return (1);
 }
