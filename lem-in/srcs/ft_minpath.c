19f21d96622bea43833626b5eba6094d0b2ac250
Alistair Norman <anorman@c5r4s3.wethinkcode.co.za>
2019-09-13 15:15:26 +0200

    made it error for no valid path

--- lem-in/srcs/ft_minpath.c
+++ lem-in/srcs/ft_minpath.c
@@ -6,7 +6,7 @@
 /*   By: anorman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/30 10:49:50 by anorman           #+#    #+#             */
-/*   Updated: 2019/09/12 09:47:24 by anorman          ###   ########.fr       */
+/*   Updated: 2019/09/13 15:15:08 by anorman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,6 +90,7 @@ char			**ft_minpath(t_room *rooms)
 		if (!temp && ++len)
 			temp = rooms;
 	}
+	ft_error(2);
 	return (NULL);
 }
 
